2bc403a123724d71e9a2538dc2aea1ac47617e4c
KotsuCoder <whoami@kotsu.red>
2026-03-27 17:08:14 -0700

    Added function to split text and lists of TextNodes based on a given delimiter, creating new TextNode objects.

--- src/main.py
+++ src/main.py
@@ -3,11 +3,16 @@ from htmlnode import HTMLNode, LeafNode
 
 
 def main():
-    test_object = TextNode("This is some anchor text", TextType.HYPERLINK,"https://boot.dev")
-    print(test_object)
-
-    test_HTML = HTMLNode("a", "Bootdev", None, {"href":"https://boot.dev"})
-    print(test_HTML)
+    test_markdown = "The **quick** brown fox _jumped_ over the `lazy` dog."
+    get_bold = split_nodes_delimiter(test_markdown, "**", TextType.BOLD)
+    get_italics = split_nodes_delimiter(get_bold, "_", TextType.ITALICS)
+    get_codeblock = split_nodes_delimiter(get_italics, "`", TextType.CODEBLOCK)
+    html = [text_node_to_html_node(i) for i in get_codeblock]
+    formatted_html = [i.to_html() for i in html]
+    final_html = ""
+    for i in formatted_html:
+        final_html += i
+    print(final_html)
 
 
 def text_node_to_html_node(text_node):
@@ -29,6 +34,25 @@ def text_node_to_html_node(text_node):
                 raise ValueError("text_node type must be a valid TextType")
     else:
         raise ValueError("text_node must be a TextNode")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if type(old_nodes) is not list:
+        first_node = TextNode(old_nodes, TextType.TEXT)
+        old_nodes = [first_node]
+    new_nodes = []
+    for node in old_nodes:
+        if node.get_type() is not TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            split_node = node.get_text().split(delimiter)
+            text_node = True
+            types = {True: TextType.TEXT, False: text_type}
+            for new_node in split_node:
+                if new_node:
+                    create_node = TextNode(new_node, types[text_node])
+                    new_nodes.append(create_node)
+                text_node = not text_node
+    return new_nodes
 
 
 if __name__ == "__main__":
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from main import split_nodes_delimiter, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -36,6 +37,19 @@ class TestTextNode(unittest.TestCase):
     def test_set_url(self):
         node = TextNode("Bootdev", TextType.HYPERLINK, "https://boot.dev")
         self.assertIsInstance(node.get_url(), str)
+    
+    def test_step_delimiter(self):
+        test_markdown = "The **quick** brown fox _jumped_ over the `lazy` dog."
+        get_bold = split_nodes_delimiter(test_markdown, "**", TextType.BOLD)
+        get_italics = split_nodes_delimiter(get_bold, "_", TextType.ITALICS)
+        get_codeblock = split_nodes_delimiter(get_italics, "`", TextType.CODEBLOCK)
+        html = [text_node_to_html_node(i) for i in get_codeblock]
+        formatted_html = [i.to_html() for i in html]
+        final_html = ""
+        for i in formatted_html:
+            final_html += i
+        expected_result = "<p>The </p><b>quick</b><p> brown fox </p><i>jumped</i><p> over the </p><code>lazy</code><p> dog.</p>"
+        self.assertEqual(final_html, expected_result)
 
 
 if __name__ == "__main__":
