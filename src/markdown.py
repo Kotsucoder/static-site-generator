3637db2e9b2d7e971ab1c39e15c7d55d71055310
KotsuCoder <whoami@kotsu.red>
2026-04-01 23:48:05 -0700

    Added function to check the type of a markdown block

--- src/markdown.py
+++ src/markdown.py
@@ -3,6 +3,30 @@ This module provides functions that manage Markdown text.
 """
 
 
+from enum import Enum
+from re import findall
+
+
+class BlockType(Enum):
+    """
+    Represents the different types that a Block can be.
+
+    Valid types include:
+        BlockType.PARAGRAPH
+        BlockType.HEADING
+        BlockType.CODE
+        BlockType.QUOTE
+        BlockType.UNORDERED_LIST
+        BlockType.ORDERED_LIST
+    """
+    PARAGRAPH = "p"
+    HEADING = "h"
+    CODE = "code"
+    QUOTE = "qt"
+    UNORDERED_LIST = "ul"
+    ORDERED_LIST = "ol"
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     """
     This function converts a full markdown string into a list of blocks.
@@ -21,3 +45,48 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         if text:
             split_markdown_valid.append(text)
     return split_markdown_valid
+
+
+def block_to_block_type(markdown_block: str) -> BlockType:
+    """
+    Takes in a markdown block and returns the block's type.
+
+    Args:
+        markdown_block: A string containing markdown text.
+    
+    Returns:
+        BlockType: The type that cooresponds to the markdown's syntax.
+    """
+
+    if markdown_block[0] == "#":
+        check = findall(r"^(?!#{7,})#{1,6}", markdown_block)
+        if check:
+            return BlockType.HEADING
+    if markdown_block[0:4] == "```\n" and markdown_block[-4:-1] == "```":
+        return BlockType.CODE
+    if markdown_block[0] == ">":
+        return BlockType.QUOTE
+    line_split = markdown_block.split("\n")
+    valid = True
+    for line in line_split:
+        if line[0:2] != "- ":
+            valid = False
+            break
+    if valid:
+        return BlockType.UNORDERED_LIST
+    valid = True
+    increment = 1
+    for line in line_split:
+        check = findall(r"^\d+\.\s", line)
+        if not check:
+            valid = False
+            break
+        num = int(check[0][:-2])
+        if num == increment:
+            increment += 1
+        else:
+            valid = False
+            break
+    if valid:
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -21,6 +21,55 @@ This is the same paragraph on a new line
         ]
         self.assertEqual(blocks, expected_result)
 
+    def test_markdown_paragraph_type(self):
+        md_block = "Hello, World!"
+        md_type = block_to_block_type(md_block)
+        self.assertEqual(md_type, BlockType.PARAGRAPH)
+    
+    def test_markdown_heading_type(self):
+        md_block = "### Heading 3"
+        md_type = block_to_block_type(md_block)
+        self.assertEqual(md_type, BlockType.HEADING)
+    
+    def test_markdown_heading_extra_type(self):
+        md_block = "####### Not a valid heading"
+        md_type = block_to_block_type(md_block)
+        self.assertEqual(md_type, BlockType.PARAGRAPH)
+    
+    def test_markdown_code_type(self):
+        md_block = """```
+Test block
+```
+"""
+        md_type = block_to_block_type(md_block)
+        self.assertEqual(md_type, BlockType.CODE)
+    
+    def test_markdown_quote_type(self):
+        md_block = "> Failure is the path to success"
+        md_type = block_to_block_type(md_block)
+        self.assertEqual(md_type, BlockType.QUOTE)
+    
+    def test_markdown_ul_type(self):
+        md_block = """- One
+- Two
+- Three"""
+        md_type = block_to_block_type(md_block)
+        self.assertEqual(md_type, BlockType.UNORDERED_LIST)
+    
+    def test_markdown_ol_type(self):
+        md_block = """1. One
+2. Two
+3. Three"""
+        md_type = block_to_block_type(md_block)
+        self.assertEqual(md_type, BlockType.ORDERED_LIST)
+    
+    def test_markdown_ol_fail_type(self):
+        md_block = """1. One
+3. Three
+5. Five"""
+        md_type = block_to_block_type(md_block)
+        self.assertEqual(md_type, BlockType.PARAGRAPH)
+
 
 if __name__ == "__main__":
     unittest.main()
